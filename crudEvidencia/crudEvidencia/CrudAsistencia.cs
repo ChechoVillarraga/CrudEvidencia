0eaa82b59f4cd1113fcf3582f1838783b528cf3e
Gildardo Aguirre <gildardoaguirrerios@gmail.com>
2016-11-10 18:50:29 -0500

    se sube formulario guia de crud

--- crudEvidencia/crudEvidencia/CrudAsistencia.cs
+++ crudEvidencia/crudEvidencia/CrudAsistencia.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace crudEvidencia
 {
-    public partial class Form1 : Form
+    public partial class CrudAsistencia : Form
     {
-        public Form1()
+        public CrudAsistencia()
         {
             InitializeComponent();
         }
